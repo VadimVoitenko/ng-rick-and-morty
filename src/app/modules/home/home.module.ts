bf46183406ae2477fa345a4a5f023c50dd689666
Vadim Voitenko <mirvoy8031@gmail.com>
2024-03-15 13:34:20 +0200

    initial commit

---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common'
+import { NgModule } from '@angular/core'
+
+import { CoreModule } from 'src/app/core/core.module'
+
+import { LayoutModule } from 'src/app/shared/modules/layout/layout.module'
+
+import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module'
+import { HomeComponent } from 'src/app/modules/home/pages/home/home.component'
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule, CoreModule, LayoutModule],
+})
+export class HomeModule {}
